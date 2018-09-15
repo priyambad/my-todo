0045e04ab14aeb9e9d0917aeabda295e542678e1
Priyambad.Pandey <priyambad.pandey@gmail.com>
2018-09-16 00:19:04 +0530

    option to delete a todo when it's complered added

--- src/Components/TodoItems.js
+++ src/Components/TodoItems.js
@@ -2,19 +2,21 @@ import React,{Component} from 'react';
 import {toggleTodoStatus, deleteTodo} from '../Actions/todoActions';
 import {connect} from 'react-redux'; 
 class TodoItems extends Component{
-    onClick(todoItem){
-        if(todoItem.completed){
-            this.props.deleteTodo(todoItem.id);
-        }
-        else {
-            this.props.toggleTodoStatus(todoItem.id);
-        }
+    onClick(id){
+        this.props.toggleTodoStatus(id);
+    }
+    onDelete (id){
+        this.props.deleteTodo(id);
     }
     render (){
         const todoItem = this.props.todoItem;
+        const closeItem = todoItem.completed ? <p className='deleteItem' onClick={this.onDelete.bind(this,todoItem.id)}> X </p> : <p></p>;
         return (
-            <div className='todoItems'onClick={this.onClick.bind(this,todoItem)}>
-                <p style={todoItem.completed ? {textDecoration: 'line-through'} : {}} >{todoItem.todo}</p>
+            <div className='todoItems'>
+                <p onClick={this.onClick.bind(this,todoItem.id)} style={todoItem.completed ? {textDecoration: 'line-through'} : {}} >
+                    {todoItem.todo}
+                </p>
+                {closeItem}
             </div>
         );
     }
